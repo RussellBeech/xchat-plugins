0e8371cd1a744cc55382381006ddab21120f1eca
RussellBeech <leonbeech@yahoo.co.uk>
2023-10-28 20:49:31 +0100

    v9.8

--- playbot_v9-8single.py
+++ playbot_v9-8single.py
@@ -10,7 +10,7 @@ import socket
 import ssl
 
 __module_name__ = "Multirpg Playbot Script"
-__module_version__ = "9.7"
+__module_version__ = "9.8"
 __module_description__ = "Multirpg Playbot Script"
 
 if sys.version_info[0] >= 3:
@@ -92,11 +92,13 @@ networklist = [ ["AyoChat",     "irc.ayochat.or.id",            False,  1,      
 		["Techtronix",  "irc.techtronix.net",           True,   1,      "+6697",        "+6697",        "multirpg@multirpg.net"],  \
 		["Techtronix",  "35.229.28.106",                True,   2,      "+6697",        "+6697",        "multirpg@multirpg.net"],  \
 		["Undernet",    "irc.undernet.org",             False,  1,      6667,           6667,           "multirpg@idlerpg.users.undernet.org"], \
-		["Undernet",    "154.35.136.18",                False,  2,      6667,           6667,           "multirpg@idlerpg.users.undernet.org"], \
+		["Undernet",    "185.117.74.172",               False,  2,      6667,           6667,           "multirpg@idlerpg.users.undernet.org"], \
+		["UnderX",      "irc.underx.org",               False,  1,      6667,           6667,           "multirpg@venus.skralg.com"], \
+		["UnderX",      "150.136.80.10",                False,  2,      6667,           6667,           "multirpg@venus.skralg.com"], \
 		["UniversalNet","irc.universalnet.org",         False,  1,      6667,           "+6697",        "multirpg@venus.skralg.com"], \
 		["UniversalNet","62.171.172.8",                 False,  2,      6667,           "+6697",        "multirpg@venus.skralg.com"], \
-		["Virtulus",    "irc.virtulus.net",             True,   1,      6667,           "+6697",        "multirpg@B790DC3F.D0CDF40.88109D7.IP"], \
-		["Virtulus",    "18.193.247.191",               True,   2,      6667,           "+6697",        "multirpg@B790DC3F.D0CDF40.88109D7.IP"] ]
+		["Virtulus",    "virtulus.be.sexy",             True,   1,      "+6697",        "+6697",        "multirpg@B790DC3F.D0CDF40.88109D7.IP"], \
+		["Virtulus",    "129.153.131.239",              True,   2,      "+6697",        "+6697",        "multirpg@B790DC3F.D0CDF40.88109D7.IP"] ]
 
 creeps.reverse()
 monsters.reverse()
